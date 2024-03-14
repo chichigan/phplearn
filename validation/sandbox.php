2203bd05bf1cfaee07b566128e9b74a0ff5b53f4
aidangan <louming123@hotmail.com>
2024-03-14 17:56:23 +0800

    Edit user_validatior.php

--- validation/sandbox.php
+++ validation/sandbox.php
@@ -2,6 +2,7 @@
 	if(isset($_POST['submit'])){
 		
 	}
+	
 ?>
 
 <!DOCTYPE html>
--- validation/user_validator.php
+++ validation/user_validator.php
@@ -1,5 +1,58 @@
 <?php
     class UserValidator{
-        public function
+        private $data;
+		private $errors = [];
+		private static $fields = ['username','email'];
+		
+		public function __construct($post_data){
+			$this->data=$post_data;
+		}
+		
+		public function validateForm(){
+			foreach (self::$fields as $field){
+				if(!array_key_exists($field,$this->data)){
+					trigger_error($field.'is not present in data');
+					return;
+				}
+			}
+			
+			$this->validateUsername();
+			$this->validateEmail();
+			return $this->errors;
+		}
+		
+		private function validateUsername(){
+			//Trim the whitespace between value
+			$val = trim($this->data['username']);
+			
+			//Check value is empty. eg ' ' only;
+			if(empty($val)){
+				$this->addError('username','username cannot be empty');
+			}
+			else{
+				if(!preg_match('/^[a-zA-Z0-9]{6,12}$/',$val)){
+					$this->addError('username','username must be 6-12 chars & alphanumeric');
+				}
+			}
+		}
+		
+		private function validateEmail(){
+			//Trim the whitespace between value
+			$val = trim($this->data['email']);
+			
+			//Check value is empty. eg ' ' only;
+			if(empty($val)){
+				$this->addError('email','Email cannot be empty');
+			}
+			else{
+				if(!filter_var($val,FILTER_VALIDATE_EMAIL)){
+					$this->addError('email','Email must be a valid email');
+				}
+			}
+		}
+		
+		private function addError($key,$val){
+			$this->errors['key']=$val;
+		}
     }
 ?>
